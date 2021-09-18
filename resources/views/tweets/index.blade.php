f36d499e331c036ac87c072a270f8daf0b52aa83
kenta <nakamori.k@gmail.com>
2021-09-18 11:23:28 +0900

    dropdown menu

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -1,77 +1,84 @@
 <x-app-layout>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 mb-3 text-right">
+    <div class="">
+        <div class="">
+            <div class="">
                 <a href="{{ url('users') }}">ユーザ一覧 <i class="fas fa-users" class="fa-fw"></i> </a>
             </div>
             @if (isset($timelines))
             @foreach ($timelines as $timeline)
-            <div class="col-md-8 mb-3">
-                <div class="card">
-                    <div class="card-haeder p-3 w-100 d-flex">
-                        <img src="{{ asset('storage/profile_image/' .$timeline->user->profile_image) }}" class="rounded-circle" width="50" height="50">
-                        <div class="ml-2 d-flex flex-column">
-                            <p class="mb-0">{{ $timeline->user->name }}</p>
-                            <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">{{ $timeline->user->screen_name }}</a>
+            <div class="">
+                <div class="">
+                    <div class="">
+                        <img src="{{ asset('storage/profile_image/' .$timeline->user->profile_image) }}" class="">
+                        <div class="">
+                            <p class="">{{ $timeline->user->name }}</p>
+                            <a href="{{ url('users/' .$timeline->user->id) }}" class="">{{ $timeline->user->screen_name }}</a>
                         </div>
-                        <div class="d-flex justify-content-end flex-grow-1">
-                            <p class="mb-0 text-secondary">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
+                        <div class="">
+                            <p class="">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
                         </div>
                     </div>
-                    <div class="card-body">
+                    <div class="">
                         {!! nl2br(e($timeline->text)) !!}
                     </div>
-                    <div class="card-footer py-1 d-flex justify-content-end bg-white">
+                    <div class="">
                         @if ($timeline->user->id === Auth::user()->id)
-                        <div class="dropdown mr-3 d-flex align-items-center">
-                            <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-ellipsis-v fa-fw"></i>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <form method="POST" action="{{ url('tweets/' .$timeline->id) }}" class="mb-0">
-                                    @csrf
-                                    @method('DELETE')
+                        <!-- alpine ドロップダウン -->
+                        <div class="flex">
+                            <div x-data="{ dropdownOpen: false }" class="relative">
+                                <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 block rounded-md bg-white p-2 focus:outline-none">
+                                    <i class="fas fa-ellipsis-v fa-fw"></i>
+                                </button>
 
-                                    <a href="{{ url('tweets/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
-                                    <button type="submit" class="dropdown-item del-btn">削除</button>
-                                </form>
+                                <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
+
+                                <div x-show="dropdownOpen" class="absolute  left-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+                                    <a href="{{ url('tweets/' .$timeline->id .'/edit') }}" class="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
+                                        編集
+                                    </a>
+                                    <form method="POST" action="{{ url('tweets/' .$timeline->id)}}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type=" submit" class="text-left w-48 px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">削除</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
+                        <!-- alpine 　ここまで-->
                         @endif
-                        <div class="mr-3 d-flex align-items-center">
+                        <div class="">
                             <a href="{{ url('tweets/' .$timeline->id) }}"><i class="far fa-comment fa-fw"></i></a>
-                            <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
+                            <p class="">{{ count($timeline->comments) }}</p>
                         </div>
-
                         <!-- ここから -->
-                        <div class="d-flex align-items-center">
+                        <div class="">
                             @if (!in_array($user->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
-                            <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
+                            <form method="POST" action="{{ url('favorites/') }}" class="">
                                 @csrf
 
                                 <input type="hidden" name="tweet_id" value="{{ $timeline->id }}">
-                                <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
+                                <button type="submit" class=""><i class="far fa-heart fa-fw"></i></button>
                             </form>
                             @else
                             <form method="POST" action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[$user->id]) }}" class="mb-0">
                                 @csrf
                                 @method('DELETE')
 
-                                <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
+                                <button type="submit" class=""><i class="fas fa-heart fa-fw"></i></button>
                             </form>
                             @endif
-                            <p class="mb-0 text-secondary">{{ count($timeline->favorites) }}</p>
+                            <p class="">{{ count($timeline->favorites) }}</p>
                         </div>
                         <!-- ここまで -->
-
                     </div>
                 </div>
             </div>
-            @endforeach
-            @endif
         </div>
-        <div class="my-4 d-flex justify-content-center">
-            {{ $timelines->links() }}
-        </div>
+        @endforeach
+        @endif
+    </div>
+    <div class="">
+        {{ $timelines->links() }}
+    </div>
     </div>
 </x-app-layout>
